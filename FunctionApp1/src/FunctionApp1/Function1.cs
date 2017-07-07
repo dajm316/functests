c63f6276b6d210bffb02c493832d828731683d58
Dino Masciotra <dmasciotra@GENETEC.COM>
2017-07-07 14:18:57 -0400

    init

---
+++ FunctionApp1/src/FunctionApp1/Function1.cs
@@ -0,0 +1,15 @@
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.ServiceBus.Messaging;
+
+namespace FunctionApp1
+{
+    public static class Function1
+    {
+        [FunctionName("ServiceBusTopicTriggerCSharp")]
+        public static void Run([ServiceBusTrigger("mytopic", "AzureFunctionsSub", AccessRights.Manage, Connection = "SBConnectionString")]string mySbMsg, TraceWriter log)
+        {
+            log.Info($"C# ServiceBus topic trigger function processed message: {mySbMsg}");
+        }
+    }
+}
